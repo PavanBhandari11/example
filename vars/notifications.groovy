2e2a879fe5c20fdb266f91a9f1483cf141d48050
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-01 16:21:35 +0530

    Update notifications.groovy

--- vars/notifications.groovy
+++ vars/notifications.groovy
@@ -3,7 +3,7 @@
 /**
  * Send notifications based on build status string
  */
-def call(String) {
+def call(buildStatus = 'STARTED') {
   // build status of null means successful
   buildStatus = buildStatus ?: 'SUCCESS'
 

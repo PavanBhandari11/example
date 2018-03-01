85035340ede2bca1ad2b92f29905b95345062bb1
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-01 15:04:23 +0530

    Update mail.groovy

--- vars/mail.groovy
+++ vars/mail.groovy
@@ -3,7 +3,14 @@
 /**
  * Send notifications based on build status string
  */
-def call(Closure body) {
+def call(Closure body)
+// Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""" {
     node('maven') {
         body()
 // Send notifications

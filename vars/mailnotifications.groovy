7056cff3a8679806dac4f1a3fbb8549834165ec8
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-02 11:15:18 +0530

    Update mailnotifications.groovy

--- vars/mailnotifications.groovy
+++ vars/mailnotifications.groovy
@@ -1,37 +1,35 @@
-def notifyStatusChangeViaEmail(buildStatus) {
-    def status
+    def notifyStatusChangeViaEmail(buildStatus) {
+        def status
 
-    switch (buildStatus) {
-        case 'SUCCESS':
-            status = 'is now green again!'
-            break
+        switch (buildStatus) {
+            case 'SUCCESS':
+                status = 'is now green again!'
+                break
 
-        case 'UNSTABLE':
-            status = 'has become unstable..'
-            break
+            case 'UNSTABLE':
+                status = 'has become unstable..'
+                break
 
-        case 'FAILURE':
-            status = 'has turned RED :('
-            break
+            case 'FAILURE':
+                status = 'has turned RED :('
+                break
+        }
+
+        emailext (
+            subject: "Job '${env.JOB_NAME}' ${status}",
+            body: "See ${env.BUILD_URL} for more details",
+            recipientProviders: [
+                [$class: 'DevelopersRecipientProvider'], 
+                [$class: 'RequesterRecipientProvider']
+            ]
+        )
     }
 
-    emailext (
-        subject: "Job '${env.JOB_NAME}' ${status}",
-        body: "See ${env.BUILD_URL} for more details",
-        recipientProviders: [
-            [$class: 'DevelopersRecipientProvider'], 
-            [$class: 'RequesterRecipientProvider']
-        ]
-    )
-}
-
-pipeline {
-    ...
-
-    post {
-        changed {
-            // Will trigger only when job status changes: GREEN -> RED, RED -> GREEN, etc
-            notifyStatusChangeViaEmail(currentBuild.currentResult)
+    pipeline {
+        post {
+            changed {
+                // Will trigger only when job status changes: GREEN -> RED, RED -> GREEN, etc
+                notifyStatusChangeViaEmail(currentBuild.currentResult)
+            }
         }
     }
-}

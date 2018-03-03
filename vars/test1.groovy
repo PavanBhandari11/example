dd500e1269ef731c93a0e744294a4417d6bb047d
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-03 21:32:14 +0530

    Create test1.groovy

---
+++ vars/test1.groovy
@@ -0,0 +1,18 @@
+def sendNotification(buildStatus) {
+  if (buildStatus != 'SUCCESS') {
+    // Do nothing - only interested when status becomes GREEN
+    return
+  }
+
+  mattermostSend "${env.JOB_NAME} has recovered! (<${env.BUILD_URL}|See the build>)"
+}
+
+pipeline {
+  ...
+
+  post {
+    changed {
+      sendNotification(currentBuild.currentResult)
+    }
+  }
+}

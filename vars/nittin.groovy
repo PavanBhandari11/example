6ba3c1efb240f014cceb8676ae537bf654a08951
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-05 21:58:49 +0530

    Update nittin.groovy

--- vars/nittin.groovy
+++ vars/nittin.groovy
@@ -1,5 +1,4 @@
     def call(body) {
-    // evaluate the body block, and collect configuration into the object
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
     body()
@@ -7,24 +6,23 @@
     def colorCode = '#FF0000'
     def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
     def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-                     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESS') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
-
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+  
+    if (buildStatus == 'STARTED') {
+      color = 'YELLOW'
+      colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESS') {
+      color = 'GREEN'
+      colorCode = '#00FF00'
+    } else {
+      color = 'RED'
+      colorCode = '#FF0000'
+    }
     node {
-        emailext (
-            to:'nitin.m@kloud9.nyc',
-            subject: 'subject',
-            body: 'details',
+      emailext (
+          to:'nitin.m@kloud9.nyc',
+          subject: 'subject',
+          body: 'details',
         )
   }
 }

9f806e295b28d7f66826495eb44eb234faef9298
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-05 22:22:11 +0530

    Update nitinm.groovy

--- vars/nitinm.groovy
+++ vars/nitinm.groovy
@@ -1,26 +1,25 @@
-def call(body) {
-// evaluate the body block, and collect configuration into the object
-body.resolveStrategy = Closure.DELEGATE_FIRST
-body.delegate = pipelineParams
-body()
-def buildStatus = 'STARTED'
-def colorName = 'RED'
-def colorCode = '#FF0000'
-def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-def summary = "${subject} (${env.BUILD_URL})"
-def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-// Override default values based on build status
-if (buildStatus == 'STARTED') {
+    def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    def buildStatus = 'STARTED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-} else if (buildStatus == 'SUCCESS') {
+    } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
-} else {
+    } else {
     color = 'RED'
     colorCode = '#FF0000'
-}
+    }
 
   node {
       emailext (

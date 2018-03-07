f2bebff5fc92ed32d54193d1831bb10c0ab71326
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-07 15:08:01 +0530

    Update Scm.groovy

--- src/org/k9/git/Scm.groovy
+++ src/org/k9/git/Scm.groovy
@@ -1,23 +1,19 @@
 package org.k9.*
 
-class Test implements Serializable {
+class Scm implements Serializable {
   def config
   def script
 
-  Test(script,config) {
+  Scm(script,config) {
     this.config = config
     this.script = script
   }
-  void wish() {
+  void git () {
     this.script.stage ('wishes'){
-    this.script.echo "stage1" + this.comfig.name
+    this.script.echo "HBD" + this.comfig.name
   }
-     this.script.stage('Preparation') { // for display purposes
-      // Get some code from a GitHub repository
-      git 'https://github.com/srinivasdevops376/springmvc-helloworld.git'
-      // Get the Maven tool.
-      // ** NOTE: This 'M3' Maven tool must be configured
-      // **       in the global configuration.           
+     this.script.stage('Git clone') {
+      git 'https://github.com/srinivasdevops376/springmvc-helloworld.git'          
       mvnHome = '/opt/maven'
       usrHome = '/home/srinivasdevops376'
    }

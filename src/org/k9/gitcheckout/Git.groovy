407abad0fdc52d3104b83742eb96d7d39546538c
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-15 10:50:54 +0530

    Create Git.groovy

---
+++ src/org/k9/gitcheckout/Git.groovy
@@ -0,0 +1,17 @@
+package org.k9.git
+import org.k9.*
+
+class git {
+  def config
+  def script
+
+  git(script,config) {
+    this.config = config
+    this.script = script
+  }
+  
+  void checkout(){   
+  this.script.stage ('git checkout') {
+  this.script.git(scm:this.config.giturl, branch:this.config.branch)
+  }
+  }
